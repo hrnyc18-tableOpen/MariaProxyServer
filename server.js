128d35d19a664113cec623d0fb8ac8fff8969617
mfosho <mc8160@gmail.com>
2018-11-02 17:05:19 -0400

    reverted without reviews component

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/restaurants', express.static(path.join(__dirname, '/public')));
 
-app.get('/restaurants/*', (req, res) => {
+app.get('/restaurants/:id', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 app.get('/api/:id', (req, res) => {
-  axios.get(`http://3.16.45.212/${req.url}`)
+  axios.get(`http://3.16.45.212/api/${req.params.id}`)
     .then(({data}) => {
       res.send(data);
     })
@@ -24,18 +24,6 @@ app.get('/api/:id', (req, res) => {
       }
     })
 })
-
-// app.get('API/Reviews/*', (req, res) => {
-//   axios.get(`http://34.207.247.29/${req.url}`)
-//     .then(({data}) => {
-//       res.send(data);
-//     })
-//     .catch((err) => {
-//       if (err) {
-//         console.log(err);
-//       }
-//     })
-// })
 
 app.listen(port, () => {
   console.log(`server running at ${port}`);
